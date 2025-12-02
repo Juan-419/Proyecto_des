9e9d3d98da180274ec144e0f107d6ae87e50d668
Juan Vega <juqnquito419@gmail.com>
2025-12-02 11:22:15 -0500

    Correccion archivo supabase

--- supa/supabase_upload.py
+++ supa/supabase_upload.py
@@ -9,21 +9,25 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 async def upload_to_bucket(file: UploadFile):
-    bucket = "Taller-mult" 
+    bucket = "Taller-mult"  # bucket real de Supabase
 
+    
     content = await file.read()
 
-    extension = os.path.splitext(file.filename)[1].lower().replace(" ", "_")
+    
+    extension = os.path.splitext(file.filename)[1].lower()
     unique_name = f"{uuid.uuid4()}{extension}"
 
+    
     file_path = f"clientes/{unique_name}"
 
+    
     result = supabase.storage.from_(bucket).upload(
         file_path,
-        content,
-        {"upsert": True}
+        content
     )
 
     print("UPLOAD RESULT:", result)
 
+    
     return f"{SUPABASE_URL}/storage/v1/object/public/{bucket}/{file_path}"
