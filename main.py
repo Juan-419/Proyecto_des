71ee5489fee6db170f54ca5d3119add619fb50df
Juan Vega <juqnquito419@gmail.com>
2025-12-02 12:35:05 -0500

    Correccion en main por error al ver la lista de clientes

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Depends, Form, HTTPException, File, UploadFile
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-
-from db import create_db_and_tables
+from sqlmodel import Session, select
+from starlette.status import HTTP_303_SEE_OTHER
+from db import create_db_and_tables, get_session
 from cliente import router as cliente_router
 from carro import router as carro_router
 from mecanico import router as mecanico_router
 from reparacion import router as reparacion_router
 from soat import router as soat_router
 from reporte import router as reportes_router
+from supa.supabase_upload import upload_to_bucket
+from supabase import create_client
 
 app = FastAPI(title="Taller de Carros API")
 
 templates = Jinja2Templates(directory="templates")
+
+# 🔥 IMPORTANTE: MONTAR templates EN EL STATE PARA QUE CLIENTE.PY LO USE
+app.state.templates = templates
+
 
 @app.on_event("startup")
 def on_startup():
@@ -26,6 +33,7 @@ app.include_router(mecanico_router, prefix="/mecanicos", tags=["Mecánicos"])
 app.include_router(reparacion_router, prefix="/reparaciones", tags=["Reparaciones"])
 app.include_router(soat_router, prefix="/soats", tags=["SOATs"])
 app.include_router(reportes_router)
+
 
 @app.get("/", response_class=HTMLResponse, tags=["Vistas HTML"])
 def root(request: Request):
