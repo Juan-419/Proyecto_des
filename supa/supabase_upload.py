6a8f6e2ca462e44ffc1b5fdaf7250b355a3c7a30
Juan Vega <juqnquito419@gmail.com>
2025-12-02 11:16:31 -0500

    Modificacion en el archivo en la parte de almacenamiento de archivos

--- supa/supabase_upload.py
+++ supa/supabase_upload.py
@@ -9,16 +9,21 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 async def upload_to_bucket(file: UploadFile):
-    bucket = "clientes"
+    bucket = "Taller-mult" 
 
     content = await file.read()
 
-
-    extension = os.path.splitext(file.filename)[1]
+    extension = os.path.splitext(file.filename)[1].lower().replace(" ", "_")
     unique_name = f"{uuid.uuid4()}{extension}"
+
     file_path = f"clientes/{unique_name}"
 
+    result = supabase.storage.from_(bucket).upload(
+        file_path,
+        content,
+        {"upsert": True}
+    )
 
-    supabase.storage.from_(bucket).upload(file_path, content)
+    print("UPLOAD RESULT:", result)
 
     return f"{SUPABASE_URL}/storage/v1/object/public/{bucket}/{file_path}"
