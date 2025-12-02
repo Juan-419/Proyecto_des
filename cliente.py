4863183fc6e7cec9d0a956ed132b700dd27731bd
Juan Vega <juqnquito419@gmail.com>
2025-12-02 13:20:09 -0500

    Pequeño error

--- cliente.py
+++ cliente.py
@@ -14,7 +14,7 @@ router = APIRouter()
 def listado_clientes_html(request: Request, session: Session = Depends(get_session)):
     clientes = session.exec(select(Cliente).where(Cliente.active == True)).all()
     return request.app.state.templates.TemplateResponse(
-        "clientes_list.html",
+        "cliente_list.html",
         {"request": request, "clientes": clientes}
     )
 
